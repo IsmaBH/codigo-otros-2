ad5e4e2815f326bb6c69987b046d50e27ea172fd
IsmaBH <deigo-b@hotmail.com>
2025-01-23 15:37:01 -0600

    Se termino el ejercicio, se refactorizo el archivo formulario.js por completo y se modificaron detalles de funcionalidad en el html

--- formulario.js
+++ formulario.js
@@ -1,103 +1,58 @@
-var formulario = document.querySelector("#form");
+//El identificador es una clase no un id y el nombre es formulario
+var formulario = document.querySelector(".formulario");
+//Los elementos del form es mejor importarlos con su id
+const invitadoNombre = document.getElementById("name");
+const invitadoEdad = document.getElementById("age");
+const invitadoNacionalidad = document.getElementById("nationality");
+//Aqui se declara el div contenedor de la lista de invitados
+const listaInvitados = document.getElementById("listaInvitados");
+//Para poder identificar al elemento de la tabla que eliminamos necesitamos una especie de id
+let id = 0;
 
-//Se crea un eventListener 
+//Se crea un eventListener refactorizado para el boton que envia a guardar al invitado
 formulario.addEventListener("submit", function(event) {
   event.preventDefault();
-  console.log(formulario.elements[0]);
+  let isValid = true;
+  if (invitadoNombre.value.length < 3){
+    invitadoNombre.style.border = "1px solid red";
+    isValid = false;
+  }
+  if (Number(invitadoEdad.value) < 18 || Number(invitadoEdad.value) > 120){
+    invitadoEdad.style.border = "1px solid red";
+    isValid = false;
+  }
+  if (isValid){
+    id += 1;
+    agregarInvitado(id,invitadoNombre.value,invitadoEdad.value,invitadoNacionalidad.value);
+    invitadoNombre.value = "";
+    invitadoEdad.value = "";
+    invitadoNombre.style.border = "";
+    invitadoEdad.style.border = "";
+    invitadoNombre.autofocus;
+  }
 });
 
-formulario.onsubmit = function(e) {
+//El boton eliminar invitado deberia ser creado a un lado del invitado que se va a eliminar
 
-  e.prevent();
-  
-  var n = formulario.elements[0]
-  var e = formulario.elements[1]
-  var na = formulario.elements[2]
-
-  var nombre = n.value
-  var edad = e.value
-
-  var i = na.selectedIndex
-  var nacionalidad = na.options[i].value
-  console.log(nombre, edad)
-  console.log(nacionalidad)
-
-  if (nombre.length === 0) {
-    n.classList.add("error")
-  }
-  if (edad < 18 || edad > 120) {
-    e.classList.add("error")
-  }
-
-if (nombre.length > 0 
-  && (edad > 18 
-    && edad < 120) ) {
-  agregarInvitado(nombre, edad, nacionalidad)
-  }
+function agregarInvitado(id,nombre, edad, nacionalidad) {
+  //Como la nacionalidad ya se tiene expandida desde el HTML ya no es necesario hacerlo aqui
+  //El elemento de la lista de invitados deberia ser obtenido desde el principio
+  //El elemento creado para los invitados deberia declarase como una variable html
+  //Como es una lista seria bueno que se agregaran en una estructura de tabla por esto 
+  //elementoInvitado sera un elemento tr y sus campos seran: Nombre,Edad,Nacionalidad y Acción
+  //Accion sera el espacio en el que se pondra el boton eliminar
+  let elementoInvitado = `<tr id="invitado_${id}">
+                            <td>${nombre}</td>
+                            <td>${edad}</td>
+                            <td>${nacionalidad}</td>
+                            <td><button onclick="eliminarInvitado(${id})" type="submit" value="${id}">Eliminar</button></td>
+                          </tr>`;
+  listaInvitados.insertAdjacentHTML("beforeend", elementoInvitado);
 }
 
-var botonBorrar = document.createElement("button")
-botonBorrar.textContent = "Eliminar invitado"
-botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
-document.body.appendChild(corteLinea)
-document.body.appendChild(botonBorrar);
-
-function agregarInvitado(nombre, edad, nacionalidad) {
-
-  if (nacionalidad === "ar") {
-    nacionalidad = "Argentina"
-  }
-  else if (nacionalidad === "mx") {
-    nacionalidad = "Mexicana"
-  }
-  else if (nacionalidad === "vnzl") {
-    nacionalidad = "Venezolana"
-  }
-  else if (nacionalidad === "per") {
-    nacionalidad = "Peruana"
-  }
-
-var lista = document.getElementById("lista-de-invitados")
-
-var elementoLista = document.createElement("div")
-elementoLista.classList.added("elemento-lista")
-lista.appendChild(elementoLista)
-
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
-spanNombre.textContent = "Nombre: "
-inputNombre.value = nombre 
-elementoLista.appendChild(spanNombre)
-elementoLista.appendChild(inputNombre)
-elementoLista.appendChild(espacio)
-
-function crearElemento(descripcion, valor) {
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
-spanNombre.textContent = descripcion + ": "
-inputNombre.value = valor 
-elementoLista.appendChild(spanNombre)
-elementoLista.appendChild(inputNombre)
-elementoLista.appendChild(espacio)
-}
-
-crearElemento("Nombre", nombre)
-crearElemento("Edad", edad)
-crearElemento("Nacionalidad", nacionalidad)
-
-
-var botonBorrar = document.createElement("button")
-botonBorrar.textContent = "Eliminar invitado"
-botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
-elementoLista.appendChild(corteLinea)
-elementoLista.appendChild(botonBorrar);
-
- botonBorrar.onclick = function() {
-// this.parentNode.style.display = 'none';
-botonBorrar.parentNode.remove()
-  }
+//Aqui se puede añadir la funionalidad de eliminar a un invitado de la lista
+//Esta funcion se activa cada que se presiona el boton del invitado que se quiere eliminar
+function eliminarInvitado(valor){
+  const invitado = document.getElementById("invitado_"+valor);
+  invitado.remove();
 }
